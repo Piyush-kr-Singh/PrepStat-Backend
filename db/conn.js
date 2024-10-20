82a0a8a40c957798d72dfee9a7baeb76ed081dbd
Rishabh Raj <rajrishabh225@gmail.com>
2024-10-21 01:29:06 +0530

    chnaged db connection to pool connection.

--- db/conn.js
+++ db/conn.js
@@ -2,18 +2,23 @@ const { config } = require('dotenv');
 const mysql=require('mysql2');
 config();
 
-const conn=mysql.createConnection({
-    user : process.env.DB_USERNAME,
-    host : process.env.DB_HOST,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_DBNAME,
-
+const conn = mysql.createPool({
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
+    waitForConnections: true,
+    connectionLimit: 10, // Max number of connections
+    queueLimit: 0       // No limit to connection queue
 });
 
-conn.connect((err)=>
-{
-    if(err) throw err;
-    console.log("Database connected successfully");
+conn.getConnection((err, connection) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Database connected successfully');
+    connection.release(); // Release the connection back to the pool
 });
 
 module.exports =conn;
